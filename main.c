e291fcb845949b3d5cdf44226cbf185c7df0eac0
Jr-Yung Tsai <wemee7012@gmail.com>
2015-03-06 17:09:30 +0800

    Create main.c

---
+++ main.c
@@ -0,0 +1,42 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+int main(){
+    int n, idx, i, check_for, printf_count;
+    int primes[25]= {2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97};
+    int *primes_count;
+    while (scanf("%d", &n) != EOF) {
+        if (n==0) break;
+
+        primes_count = calloc(100, sizeof(int));
+        
+        for (i=2; i<=n; i++) {
+            idx = 0;
+            check_for = i;
+            while (primes[idx]<=check_for && idx<=25) {
+                if (check_for%primes[idx] == 0) {
+                    primes_count[primes[idx]]++;
+                    check_for /= primes[idx];
+                    continue;
+                }
+                idx++;
+            }
+        }
+        
+        printf("%3d! =", n);
+        printf_count = 0;
+        for (i=0; i<100; i++) {
+            if (primes_count[i] != 0) {
+                if (printf_count >= 15) {
+                    printf("\n      ");
+                    printf_count = 0;
+                }
+                printf("%3d", primes_count[i]);
+                printf_count++;
+            }
+        }
+        printf("\n");
+        free(primes_count);
+    }
+    return 0;
+}
